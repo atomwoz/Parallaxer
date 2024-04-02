ee25813fc516c7e77962864f10932c372673f5f6
atomwoz <atwozniak67@gmail.com>
2024-04-02 19:58:02 +0200

    Added simple parallax

---
+++ src/ascii_art.rs
@@ -0,0 +1,52 @@
+use std::cmp::min;
+pub fn slice_multiline<'a>(lines: &Vec<&'a str>, start: usize, end: usize) -> Vec<String> {
+    let mut to_ret = Vec::new();
+    for x in lines {
+        let chars = x.chars().collect::<Vec<_>>();
+        to_ret.push(chars[start..end].iter().cloned().collect::<String>());
+    }
+    to_ret
+}
+
+pub fn get_multiline_width(lines: &Vec<&str>) -> usize {
+    let mut min_width = usize::MAX;
+    for x in lines {
+        min_width = min(x.chars().count(), min_width);
+    }
+    min_width
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_slice_multiline() {
+        let lines = vec![
+            "Hello,",
+            "World!",
+            "This is",
+            "a test",
+        ];
+        let start = 2;
+        let end = 6;
+        let expected = vec![
+            "llo,",
+            "rld!",
+            "is",
+            " test",
+        ];
+        assert_eq!(slice_multiline(&lines, start, end), expected);
+    }
+
+    #[test]
+    fn test_get_multiline_width() {
+        let lines = vec![
+            "Hello",
+            "World!",
+            "This is a test",
+        ];
+        let expected = 5;
+        assert_eq!(get_multiline_width(&lines), expected);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use std::{iter, thread, time::Duration};
+
+mod ascii_art;
+mod data;
+
+fn cls_console() {
+    if cfg!(windows) {
+        let _ = std::process::Command::new("cmd")
+            .args(&["/C", "cls"])
+            .status();
+    } else {
+        print!("{}[2J", 27 as char);
+    }
+}
+
+fn set_cursor_position(x: u16, y: u16) {
+    let _ = crossterm::execute!(std::io::stdout(), crossterm::cursor::MoveTo(x, y));
+}
+
+const TERMINAL_WIDTH: usize = 80;
+
 fn main() {
-    println!("Hello, world!");
+    let lines: Vec<&str> = data::TUNNEL.lines().collect();
+    let mut i = 0;
+    cls_console();
+    loop {
+        let sliced = ascii_art::slice_multiline(&lines, i, i + TERMINAL_WIDTH);
+        set_cursor_position(0, 0);
+        for x in sliced {
+            println!("{}", x);
+        }
+        i += 1;
+        if (i + TERMINAL_WIDTH >= ascii_art::get_multiline_width(&lines)) {
+            i = 0;
+        }
+        thread::sleep(Duration::from_millis(20));
+    }
 }
